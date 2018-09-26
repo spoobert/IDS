ae378153aff291cf1eded2ced489ee4fd3e9e1e7
devin t brown <toel3my@gmail.com>
2018-09-25 21:11:31 -0700

    bfs lookup

--- projTwo.py
+++ projTwo.py
@@ -19,31 +19,37 @@ def isSolution( arr ):
         if arr[i] > arr[i + 1]:
             return False
     return  True
-
+#arr[0][0,1,2,3] 0=lbound ; 1=rbound ; 2=parent ; 3=self
+#function assumes arr[0]=(0,0,anyVal,0)
 def lalDoesBFS( arr ):
     q = qu()
-    q.put( arr )
     pointers = {}
     values = []
     index = 0
-    pointers[str(arr)] = -1
+    arr[0][2] = -1
+    q.put( arr )
     values.append(arr)
+    #parent of -1 means root node
+    pointers[ arr[0][2] ] = index
     while( True ):
         current = q.get()
         print( current )
         if isSolution( current ):
             return (current, values, pointers)
         children = NewPermute( current )
+        chilCount = 0
         for child in children:
+            chilCount += 1 
+            #parent is set
+            child[0][2] = index
+            #self index is set
+            child[0][3] = index + chilCount
             q.put( child )
-            #pointers[str(child)] = index
-            child[0][2] = len(values)
-            #print(str(child))
             values.append(child)
-            pointers[len(values)-1] = index
+            #key=self index ; value=parent index
+            pointers[child[0][3]] = child[0][2]
+            chilCount += 1
         index += 1
-        
-
 
 def NewPermute(a):
     b = []
@@ -51,7 +57,7 @@ def NewPermute(a):
         for I in range(1,len(a) - (N)):
             if(a[0][0] != N  or a[0][1] != N+I):
                 tmp = revItoJ(a, N, N+I)
-                tmp[0] = (N,N+I, -1)
+                tmp[0] = (N , N+I , 0 , 0)
                 b.append(tmp)
     return b
 
@@ -78,7 +84,7 @@ def lalDoesIDS( arr ):
 
 
 def main():
-    arr = [(0,0,0),3,1,4,7,6, 3, 9]
+    arr = [(0,0,0,0),3,1,4,7,6, 3, 9]
     node, vals, dic = lalDoesBFS(arr)
     idx = dic[node[0][2]]
     while( idx >= 0):
