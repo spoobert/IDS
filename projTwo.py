357dcde255be8489ea26cff8b31a5c310add5181
devin t brown <toel3my@gmail.com>
2018-09-26 10:01:46 -0700

    DFS start

--- projTwo.py
+++ projTwo.py
@@ -61,6 +61,44 @@ def lalDoesBFS( arr ):
         index += ( chilCount) # +1
         #index += 1
 
+
+def lalDoesDFS( arr , d):
+    stack = []
+    pointers = {}
+    values = []
+    index = 0
+    arr[0][2] = -1
+    arr[0][3] = 0 
+    stack.append( arr )
+    values.append( arr )
+    #parent of -1 means root node
+    pointers[ arr[0][3] ] = index
+    #pointers[ arr[0][3] ] = arr[0][2]
+    while( True ):
+        current = stack.pop()
+        if isSolution( current ):
+            return (current, values, pointers)
+        print(current)
+        children = NewPermute( current )
+        chilCount = 0
+        for child in children:
+            chilCount += 1 
+            #parent is set
+            child[0][2] = index
+            #self index is set
+            child[0][3] = index + chilCount
+            stack.append( child )
+            values.append(child)
+            #key=self index ; value=parent index
+            pointers[child[0][3]] = child[0][2]
+            if isSolution( child ):
+                print('sol ', child)
+                return True 
+            print(child,' child')
+        index += ( chilCount )
+
+
+
 def NewPermute(a):
     b = []
     #print(a)
@@ -74,17 +112,8 @@ def NewPermute(a):
                 #print(tmp)
                 b.append(tmp)
     return b
-
-
         
 
-def lalDoesDFS( arr, d ):
-    print( arr )
-    if d == 0:
-        return isSolution( arr )
-    children = pu( arr )
-    for c in children:
-        return lalDoesDFS( c, d - 1 )
 
 def lalDoesIDS( arr ):
     d = 0 
@@ -100,19 +129,38 @@ def lalDoesIDS( arr ):
 def main():
     arr = []
     arr.append( [0,0,0,0] )
-    arr.append( [3,1,4,7,6, 3, 9] )
+    arr.append( [3,2,7,1,9,4] )    
+    print( lalDoesDFS( arr , 1000 ) )
+    '''
+    a = NewPermute( arr )[0]
+    print('root',arr)
+    print(a)
+    b = NewPermute( a )[0]
+    print(b)
+    c = NewPermute( b )[0]
+    print(c)
+    '''
+
+
+    '''
+    arr = []
+    arr.append( [0,6,0,0] )
+    arr.append( [3,1,4,7,6, 3, 9] )        
+    print('test: ', getChildren(arr) )
+    '''
+    '''
     node, vals, dic = lalDoesBFS(arr)    
-    idx = dic[node[0][2]]
-    print('solution: ',node)
+    idx = dic[node[0][3]]
+    print('solution: ',node[1] )
     while( idx >= 0 ):        
 
         node = vals[idx]
         if node[0][2] == -1:
             print('root: ',node[1])
             break
-        print(node[1])        
-        idx = dic[node[0][2]]
-        
+        print(node[1])
+        idx = dic[node[0][3]]
+    '''
 
     #print( lalDoesIDS( arr ) )    
     #print( lalDoesDFS( arr, 10 ) )
